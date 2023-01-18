d43a6faf01b6bb648ad57d10bae62296b1cc899e
ninjacoolers <top1successfulman@gmail.com>
2023-01-19 00:55:52 +1000

    initial commit

---
+++ src/controllers/category.controller.js
@@ -0,0 +1,77 @@
+const db = require("../models");
+const Category = db.category;
+const config = require("../config/index");
+
+exports.create = (req, res) => {
+  const category = new Category({
+    name: req.body.name,
+    user: req.userId
+  })
+
+  category.save(async (err, _category) => {
+    if (err) {
+      res.status(400).send({ message: err, status: config.RES_STATUS_FAIL });
+      return;
+    }
+
+    return res.status(200).send({
+      message: config.RES_MSG_SAVE_SUCCESS,
+      data: _category,
+      status: config.RES_STATUS_SUCCESS,
+    });
+  });
+}
+
+exports.getAll = (req, res) => {
+  Category.find()
+    .exec((err, categories) => {
+
+      if (err) {
+        res.status(400).send({ message: err, status: config.RES_STATUS_FAIL });
+        return;
+      }
+
+      if (!categories) {
+        return res.status(404).send({ message: config.RES_MSG_DATA_NOT_FOUND, status: config.RES_STATUS_SUCCESS });
+      }
+
+      return res.status(200).send({
+        message: config.RES_MSG_DATA_FOUND,
+        data: categories,
+        status: config.RES_STATUS_SUCCESS,
+      });
+    })
+};
+
+exports.update = (req, res) => {
+  Category.updateOne({ _id: req.params.id }, {name: req.body.name})
+    .exec(async (err, category) => {
+
+      if (err) {
+        res.status(500).send({ message: err, status: config.RES_MSG_UPDATE_FAIL });
+        return;
+      }
+
+      return res.status(200).send({
+        message: config.RES_MSG_UPDATE_SUCCESS,
+        data: category,
+        status: config.RES_STATUS_SUCCESS,
+      });
+    })
+};
+
+
+exports.delete = (req, res) => {
+  Category.deleteOne({ _id: req.params.id })
+  .exec((err) => {
+
+    if (err) {
+      res.status(500).send({ message: err, status: config.RES_MSG_DELETE_FAIL });
+      return;
+    }
+    return res.status(200).send({
+      message: config.RES_MSG_DELETE_SUCCESS,
+      status: config.RES_STATUS_SUCCESS,
+    });
+  })
+};
